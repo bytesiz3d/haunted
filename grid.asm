3aded4f5ff90e8a0f1c5993ae493c39505ebba39
SaadBadr <saadbadr23@gmail.com>
2019-11-26 20:42:19 +0200

    GridV2

--- grid.asm
+++ grid.asm
@@ -1,40 +1,95 @@
-                    .MODEL SMALL
+                    .MODEL LARGE
 ;------------------------------------------------------
                     .STACK 64      
-
 ;------------------------------------------------------                    
 
                     .DATA
 
+WindowResolution_X			EQU  320
+WindowResolution_Y			EQU  200
+GRIDCOLUMNS					EQU  8
+GRIDROWS					EQU  5  
+CellResolution_X			DW  ?	
+CellResolution_Y			DW  ?
+
+; TO GET GRID WITH SQUARE CELLS : GRIDCOLUMNS / GRIDROWS = WindowResolution_X / WindowResolution_Y
 
 ;--------------------------------------------------------
+
+
+
+
                     .CODE                                                 
 MAIN                PROC FAR        
                     MOV AX,@DATA    
                     MOV DS,AX     
                     
+					
 					MOV AH,0
 					MOV AL,13H
 					INT 10H
 					                   
+					
+					CALL DRAWGRID
+					
+					  
+                    
+                    MOV AH,4CH 
+                    INT 21H
+
+MAIN                ENDP
+;-------------------------------------------------
+
+
+
+DrawGrid                PROC NEAR
+
+					
+					
+					PUSH AX
+					PUSH CX
+					PUSH DX
+					PUSH BX
+					PUSH SP
+					PUSH BP
+					PUSH SI
+					PUSH DI
+					
+					MOV AX,WindowResolution_X
+					MOV DX,0
+					MOV CX,GRIDCOLUMNS               
+					DIV CX
+					MOV CellResolution_X,AX
+					
+					
+					MOV AX,WindowResolution_Y   
+					MOV DX,0
+					MOV CX,GRIDROWS               
+					DIV CX
+					MOV CellResolution_Y,AX
 					MOV AL,5
 					MOV AH,0ch
-	
-					MOV CX,16
+					
+					MOV CX,CellResolution_X
+					
 					COLUMNLOOP1:
 					MOV DX,0
 									
 					COLUMNLOOP2:
 					    INT 10H
 					    INC DX
-						CMP DX,200
+						CMP DX,WindowResolution_Y
 						JL COLUMNLOOP2
 					
-					ADD CX,16
-					CMP CX,320
+					ADD CX,CellResolution_X
+					CMP CX,WindowResolution_X
 					JL COLUMNLOOP1
 					
-					MOV DX,10
+					
+					
+					MOV DX,CellResolution_Y
+					
+					
 					
 					ROWLOOP1:
 					MOV CX,0
@@ -42,17 +97,28 @@ MAIN                PROC FAR
 					ROWLOOP2:
 					    INT 10H
 					    INC CX
-						CMP CX,320
+						CMP CX,WindowResolution_X
 						JL ROWLOOP2
 					
-					ADD DX,10
-					CMP DX,200
+					ADD DX,CellResolution_Y
+					CMP DX,WindowResolution_Y
 					JL ROWLOOP1
 					
+					POP DI
+					POP SI
+					POP BP
+					POP SP
+					POP BX
+					POP DX
+					POP CX
+					POP AX
 					
-                    MOV AH,4CH 
-                    INT 21H
+					
+                    RET
+					
+DrawGrid                ENDP    
 
-MAIN                ENDP
-;-------------------------------------------------
+
+
+;--------------------------------------------------
                     END MAIN        
